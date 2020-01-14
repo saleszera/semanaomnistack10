de3679f064a425f3b4ecac562ff4386d38fb23f1
saleszera <crowzcoleslaw@gmail.com>
2020-01-14 14:05:37 -0300

    iniciado backend da aplicação com a criação e conexão com banco de dados, também foram inseridas funções como, listagem, inserção (podem ser visualizadas em src/controller/DevController.js) e procura de usuarios proximos (pode ser visualizado em src/controller/SearchController.js), em src/routes,js podem ser visualizadas as rotas criadas. Também foi criada uma função para transformar uma string em array.

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0-ks7nx.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+//HTTP methods: GET, POST, PUT and DELETE
+
+//Params types
+//Query params: req.query(filters, pagination, ordenation, ...)
+//Route params: req.params(identify a resource in modification or removal)
+//Body: req.body(data for creation or modification a resgistry )
+
+
+
+app.listen(3333);
